5eea6c1c36fddc7cd668a952e6760e404e50ec2f
Samarth Rawat <samarthrawat@Samarths-MacBook-Air.local>
2026-02-01 19:25:02 +0530

    Updated main.jsx to make home page visible

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
+
 
 
