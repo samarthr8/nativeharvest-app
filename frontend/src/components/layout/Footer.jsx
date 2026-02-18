81f075c0e5d0dcc136a7aeaeecbd7bbf0a49b475
Samarth Rawat <samarthrawat@Samarths-MacBook-Air.local>
2026-02-18 16:01:28 +0530

    Changed transparent logo

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo-horizontal.png";
+import logo from "../../assets/NH-Logo-Old-Transparent.png";
 
 const Footer = () => {
   return (
